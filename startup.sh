004fa2793fe5ba2dc94495972616108e7465fdf6
cs <csheehan630@gmail.com>
2022-04-09 04:15:47 -0400

    fixing startup.sh

--- startup.sh
+++ startup.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
-if [ $EUID != 0 ]; then
-    sudo "$0" "$@"
-    exit $?
-fi
+echo "sudo (y) non-sudo (n) [y/n]?"
+read sVar
 
-if [ -f "/etc/arch-release" ]
+
+if [ $sVar == y] 
 then
-	sudo pacman -Syyu git vim tmux python3 openssh-server openvpn easy-rsa yay
-	yay -S autokey-gtk
+	sudo "$0" "$@"
+	if [ -f "/etc/arch-release" ]
+	then
+		sudo pacman -Syyu git vim tmux python3 openssh-server openvpn easy-rsa yay
 
-elif [ -f "/etc/debian_version" ]
-then
-	sudo apt update && sudo apt upgrade
-	sudo apt install git vim tmux python3 openssh-server openvpn easy-rsa
+	elif [ -f "/etc/debian_version" ]
+	then
+		sudo apt update && sudo apt upgrade
+		sudo apt install git vim tmux python3 openssh-server openvpn easy-rsa
 
+	else
+		echo "not an arch or debian distro"
+		exit 1
+	fi
 else
-	echo "not an arch or debian distro"
-	exit 1
-fi
-
-mkdir ~/git 
-git clone https://github.com/conacts/files-to-transfer ~/git
-git mv ~/git/files-to-transfer/.vimrc ~/git/files-to-transfer/.tmux.conf ~/git/files-to-transfer/.bashrc
+	mkdir ~/git 
+	cd git
+	git clone https://github.com/conacts/files-to-transfer 
+	mv files-to-transfer/config/.vimrc files-to-transfer/config/.tmux.conf files-to-transfer/config/.bashrc ~
+fi 
+exit 1
