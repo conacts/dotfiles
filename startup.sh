b9d6a93cbd30ce28958b8d7a5774251a5948473a
cs <csheehan630@gmail.com>
2023-08-23 19:42:57 -0400

    adding nvim ppa for ubuntu

--- startup.sh
+++ startup.sh
@@ -1,68 +1,36 @@
 #!/bin/bash
 
-# Check for verbose mode
+### Global Section ###
 VERBOSE=${VERBOSE:-0}
+
 log() {
     if [ "$VERBOSE" -eq 1 ]; then
         echo "$@"
     fi
 }
 
-# Function to install Homebrew on macOS
-install_brew() {
-    log "Installing Homebrew..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-}
-
-# Function to install packages on macOS
-install_macos_packages() {
-    log "Installing packages on macOS..."
-    brew install git neovim tmux htop tree neofetch python3 openvpn
-    brew install --cask karabiner-elements
-}
-
-# macOS configuration
-macos_configuration() {
-    log "Configuring macOS..."
-    defaults write com.apple.desktopservices DSDoneWriteNetworkStores true
-    defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
-}
-
-# Function to update Arch Linux
-update_arch() {
-    log "Updating Arch Linux..."
-    sudo pacman -Syyu
-}
-
-# Function to install packages on Arch Linux
-install_arch_packages() {
-    log "Installing packages on Arch Linux..."
-    sudo pacman -S git neovim tmux htop tree neofetch python openvpn openssh yay
-}
-
-# Function to update Debian-based Linux distributions
-update_debian() {
-    log "Updating Debian-based Linux..."
-    sudo apt update
-}
-
-# Function to install packages on Debian-based Linux distributions
-install_debian_packages() {
-    log "Installing packages on Debian-based Linux..."
-    sudo apt install -y git neovim tmux htop tree neofetch python3 python3-pip openvpn openssh-server
-}
-
-# Function to clone the specified repositories for macOS
-clone_macos_repositories() {
-    log "Cloning macOS repositories..."
-    local target_dir=~/.config/karabiner/assets/complex_modifications
-    if [ -e "$target_dir" ]; then
-        mv "$target_dir" "$target_dir.bak"
+move_dotfiles() {
+    log "Moving dotfiles..."
+    local config_dir="./config"
+    if [ -d "$config_dir" ]; then
+        for file in "$config_dir"/.*; do
+            if [ -f "$file" ]; then
+                local filename=$(basename "$file")
+                local destination="$HOME/$filename"
+                if [ -e "$destination" ]; then
+                    log "Conflict: File '$filename' already exists in the destination. Renaming to '$filename.bak'"
+                    mv "$destination" "$destination.bak"
+                fi
+                log "Moving '$filename' to '$HOME/'"
+                cp "$file" "$HOME/"
+            fi
+        done
+    else
+        log "Error: 'dotfiles/config' directory not found or is not a directory."
+        exit 1
     fi
-    git clone https://github.com/conacts/karabiner "$target_dir"
 }
 
-# Function to clone the specified repositories for Neovim
 clone_neovim_repository() {
     log "Cloning Neovim repository..."
     local target_dir=~/.config/nvim
@@ -78,7 +46,6 @@ clone_neovim_repository() {
     git clone --depth 1 https://github.com/wbthomason/packer.nvim "$packer_dir"
 }
 
-# Function to clone Tmux Plugin Manager (tpm)
 clone_tmux_tpm() {
     log "Cloning Tmux Plugin Manager (tpm)..."
     local target_dir=~/.tmux/plugins/tpm
@@ -88,39 +55,67 @@ clone_tmux_tpm() {
     git clone https://github.com/tmux-plugins/tpm "$target_dir"
 }
 
-# Function to move dotfiles to their proper directories
-move_dotfiles() {
-    log "Moving dotfiles..."
-    local config_dir="./config"
-    
-    # Check if the "config" directory exists
-    if [ -d "$config_dir" ]; then
-        # Loop through each file in the "config" directory
-        for file in "$config_dir"/.*; do
-            # Check if the file is a regular file (not a directory)
-            if [ -f "$file" ]; then
-                local filename=$(basename "$file")
-                local destination="$HOME/$filename"
-                
-                # Check if a file with the same name already exists in the destination
-                if [ -e "$destination" ]; then
-                    log "Conflict: File '$filename' already exists in the destination. Renaming to '$filename.bak'"
-                    mv "$destination" "$destination.bak"
-                fi
-                
-                log "Moving '$filename' to '$HOME/'"
-                cp "$file" "$HOME/"
-            fi
-        done
-    else
-        log "Error: 'dotfiles/config' directory not found or is not a directory."
-        exit 1
-    fi
+
+### macOS Section ###
+install_brew() {
+    log "Installing Homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 }
 
-# Check the operating system and perform the appropriate action
+install_macos_packages() {
+    log "Installing packages on macOS..."
+    brew install git neovim tmux htop tree neofetch python3 openvpn
+    brew install --cask karabiner-elements
+}
+
+macos_configuration() {
+    log "Configuring macOS..."
+    defaults write com.apple.desktopservices DSDoneWriteNetworkStores true
+    defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
+}
+
+clone_macos_repositories() {
+    log "Cloning macOS repositories..."
+    local target_dir=~/.config/karabiner/assets/complex_modifications
+    if [ -e "$target_dir" ]; then
+        mv "$target_dir" "$target_dir.bak"
+    fi
+    git clone https://github.com/conacts/karabiner "$target_dir"
+}
+
+
+### Ubuntu/Debian Section ###
+update_debian() {
+    log "Updating Debian-based Linux..."
+    sudo apt update
+}
+
+add_debian_ppas() {
+    log "Adding PPAs on Debian-based Linux..."
+    sudo add-apt-repository -y ppa:neovim-ppa/unstable
+    sudo apt update
+}
+
+install_debian_packages() {
+    log "Installing packages on Debian-based Linux..."
+    sudo apt install -y git neovim tmux htop tree neofetch python3 python3-pip openvpn openssh-server
+}
+
+
+### Arch Section ###
+update_arch() {
+    log "Updating Arch Linux..."
+    sudo pacman -Syyu
+}
+
+install_arch_packages() {
+    log "Installing packages on Arch Linux..."
+    sudo pacman -S git neovim tmux htop tree neofetch python openvpn openssh yay
+}
+
+
+### Operating System Detection and Execution ###
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    # macOS
     log "Detected macOS"
     if [ -x "$(command -v brew)" ]; then
         log "Homebrew is already installed"
@@ -131,22 +126,24 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     macos_configuration
     clone_macos_repositories
     move_dotfiles
+
 elif [ -f "/etc/arch-release" ]; then
-    # Arch Linux
     log "Detected Arch Linux"
     update_arch
     install_arch_packages
     clone_neovim_repository
     clone_tmux_tpm
     move_dotfiles
+
 elif [ -f "/etc/debian_version" ] || [ -f "/etc/debian_release" ]; then
-    # Debian-based Linux
     log "Detected Debian-based Linux"
     update_debian
+    add_debian_ppas
     install_debian_packages
     clone_neovim_repository
     clone_tmux_tpm
     move_dotfiles
+
 else
     log "Unsupported operating system"
 fi
